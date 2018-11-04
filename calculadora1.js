296664d48b444488b39f19db09d5fb78ce63d18c
Marta Gomes <44323503+martatfgomes@users.noreply.github.com>
2018-11-04 18:19:58 +0000

    Add files via upload

---
+++ calculadora1.js
@@ -0,0 +1,31 @@
+function sum() {
+let n1,n2
+n1=Number(document.querySelector("#num1").value)
+n2=Number(document.querySelector("#num2").value)
+
+alert(n1+n2)
+}
+
+function sub (){
+    let n3,n4
+    n3=Number(document.querySelector("#num3").value)
+    n4=Number(document.querySelector("#num4").value)
+    
+    alert(n3-n4)
+    }
+
+    function mult (){
+        let n5,n6
+        n5=Number(document.querySelector("#num5").value)
+        n6=Number(document.querySelector("#num6").value)
+        
+        alert(n5*n6)
+        }
+
+        function div() {
+            let n7,n8
+            n7=Number(document.querySelector("#num7").value)
+            n8=Number(document.querySelector("#num8").value)
+            
+            alert(n7/n8)
+            }
